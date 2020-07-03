153f1f5fc9348a5264815770107320057c5bb145
Anant Kanchan <anant4265@gmail.com>
2020-07-03 19:24:32 +0530

    initial commit message

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime
 {
@@ -23,7 +24,9 @@ class WorldTime
       DateTime now = DateTime.parse(datetime);
       now=now.add(Duration(hours: int.parse(offset) ));
 
-      time=now.toString();
+     // time=now.toString();
+      time=DateFormat.jm().format(now);
+
 
     }
     catch(e ){
