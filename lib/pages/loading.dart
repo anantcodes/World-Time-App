4a07d49d90dedc3e04e8885b81e8aa33049c9fdf
Anant Kanchan <anant4265@gmail.com>
2020-07-03 13:53:33 +0530

    initial commit message

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -16,6 +16,9 @@ class _LoadingState extends State<Loading> {
     WorldTime instance=WorldTime(location: 'Berlin',flag: 'germany.png',url:'Europe/Berlin');
     await instance.getTime();
     print(instance.time);
+    setState(() {
+      time=instance.time;
+    });
   }
 
 
@@ -29,7 +32,10 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
