e407a5e5da7e6fc3a1532234f581a2eab73e16f9
Anant Kanchan <anant4265@gmail.com>
2020-07-04 12:36:24 +0530

    initial commit message

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,8 +42,16 @@ print(data);
                   onPressed: () {
                     Navigator.pushNamed(context, '/location');
                   },
-                  icon: Icon(Icons.edit_location),
-                  label: Text('Edit location'),
+                  icon: Icon(
+                      Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                      'Edit location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                  ),
                 ),
                 SizedBox(height: 20.0),
                 Row(
@@ -54,6 +62,7 @@ print(data);
                       style: TextStyle(
                         fontSize: 28.0,
                         letterSpacing: 2.0,
+                        color: Colors.white,
                       ),
                     ),
                   ],
@@ -63,6 +72,7 @@ print(data);
                   data['time'],
                   style: TextStyle(
                     fontSize: 66.0,
+                    color: Colors.white,
                   ),
                 )
               ],
