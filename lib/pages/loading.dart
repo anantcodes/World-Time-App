26da9f8e484f97170237d866d7c1568e2864370a
Anant Kanchan <anant4265@gmail.com>
2020-07-04 11:48:17 +0530

    initial commit message

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -38,9 +39,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 80.0,
+        ),
       ),
     );
   }
